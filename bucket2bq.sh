bd5fd3f9ffc6b6b58763b35aa101d05ec29b4bbf
Dominic Zippilli <35895831+domZippilli@users.noreply.github.com>
2019-10-02 12:25:42 -0700

    Add set -eou pipefail

--- bucket2bq.sh
+++ bucket2bq.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -eou pipefail
+
 # Display script usage
 function usage(){
     echo >&2
