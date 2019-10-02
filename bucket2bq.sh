d0b4847436a8ea9e896de4ed5e0dab7e257e88d6
Dominic Zippilli <35895831+domZippilli@users.noreply.github.com>
2019-10-02 12:34:49 -0700

    Update bucket2bq.sh

--- bucket2bq.sh
+++ bucket2bq.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eou pipefail
+set -euo pipefail
 
 # Display script usage
 function usage(){
